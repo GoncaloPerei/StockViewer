b11ef9541851525a05235304efa51743d8aa9551
Gonçalo Pereira <83283563+GoncaloPerei@users.noreply.github.com>
2023-01-08 22:40:54 +0000

    Feature 1 - Ficheiro do Programa
    Colocação do ficheiro cpp do programa no repositório!

---
+++ program.cpp
@@ -0,0 +1,14 @@
+/************************************************************************
+                            Projeto Final AED
+        StockViewer - Programa de Gestão de Stock de uma Garrafeira
+ Trabalho Realizado Por: Gonçalo Pereira, Gonçalo Freitas, Pedro Marques
+************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello World");
+
+    return 0;
+}
